7eed223bfeab25e5db02c4e4af6a0ad4480638ec
bronx92 <especialcontas@outlook.com>
2021-11-05 00:34:12 -0300

    Conclui exibiçao de detalhes

--- src/app/pages/details/containers/details.page.ts
+++ src/app/pages/details/containers/details.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CityDailyWeather } from 'src/app/shared/models/weather.model';
+import { AppState } from 'src/app/shared/state/app.reducer';
+import * as fromDetailsAction from 'src/app/pages/details/state/details.actions';
+import * as fromDetailsSelectors from '../state/details.selectors';
+
 
 @Component({
   selector: 'jv-details',
@@ -7,9 +14,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  details$: Observable<CityDailyWeather>;
+  loading$: Observable<boolean>;
+  error$: Observable<boolean>;
+
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+
+    this.store.dispatch(fromDetailsAction.loadWeatherDetails());
+    this.details$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsEntity));
+    this.loading$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsLoading));
+    this.error$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsError));
   }
 
 }
